346a3be1fd4e9b4d604115df6d5d36c1c76694a0
mariaclarabarbosa <maria.estrella@neki-it.com.br>
2021-09-24 18:47:32 -0300

    Ajustes

--- App.js
+++ App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { StatusBar, Platform } from 'react-native';
 import Home from './src/pages/Home';
 
 export default function App() {
   return (
-    <Home />
+    <>
+      <StatusBar 
+        barStyle='light-content' 
+        backgroundColor={Platform.OS === 'android' ? '#121015' : undefined} 
+      />
+      <Home />
+    </>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { 
   View, 
   Text, 
   StyleSheet, 
   TextInput, 
   Platform,
-  FlatList
+  FlatList,
 } from 'react-native';
 import Button from '../components/Button';
 import SkillCard from '../components/SkillCard';
@@ -13,14 +13,28 @@ import SkillCard from '../components/SkillCard';
 export default function Home() {
   const [novaSkill, setNovaSkill] = useState('');
   const [minhasSkills, setMinhasSkills] = useState([]);
+  const [saudacao, setSaudacao] = useState('');
 
   function handleAdicionarNovaSkill() {
     setMinhasSkills(oldState => [...oldState, novaSkill]);
+    setNovaSkill('');
   }
+
+  useEffect(() => {
+    const horaAtual = new Date().getHours();
+    if (horaAtual < 12) {
+      setSaudacao('Bom Dia!');
+    } else if (horaAtual >= 12 && horaAtual < 18) {
+      setSaudacao('Boa Tarde!');
+    } else {
+      setSaudacao('Boa Noite!');
+    }
+  }, [])
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hello World!</Text>
+      <Text style={styles.title}>Bem-Vinda</Text>
+      <Text style={styles.gretting}>{saudacao}</Text>
       <TextInput
         style={styles.input}
         placeholder='Nova Skill'
@@ -64,5 +78,8 @@ const styles = StyleSheet.create({
     padding: Platform.OS === 'ios' ? 15 : 10,
     marginTop: 30,
     borderRadius: 7,
+  },
+  gretting: {
+    color: '#FFF',
   },
 });
